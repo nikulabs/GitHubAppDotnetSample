b3da8b4a70bf2608ee700a757d61ee24a680aca2
Ben Nikula <nikulabs@gmail.com>
2020-03-16 10:50:05 +0000

    Fix type of pattern

--- Models/RuleItem.cs
+++ Models/RuleItem.cs
@@ -3,7 +3,7 @@ namespace GitHubAppDotnetSample.Models
     public class RuleItem
     {
         public string Name { get; set; }
-        public bool Pattern { get; set; }
+        public string Pattern { get; set; }
         public string Message { get; set; }
     }
 }
